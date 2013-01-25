46f24dd6ef472ad58081e7bc880afc5b429a8121
Kevin Harriss <special.kevin@gmail.com>
2013-01-25 16:13:48 -0600

    Added it so you can upload all the files from a dir.

--- inexcess/backup.py
+++ inexcess/backup.py
@@ -1,8 +1,14 @@
+import os
+
 import inexcess
 from inexcess import mongo
+
 
 def backupFiles():
     backup_paths = inexcess.read_config('paths')
     for key, path in backup_paths:
-        mongo.uploadFile(path, 'files')
+        #THIS CRAZY HACK LOOKING THING IS TO GET ME ALL THE FILES
+        files = list(itertools.chain(*[files for root, dirs, files in os.walk(path)]))
+        for single_file in files:
+            mongo.uploadFile(single_file, 'files')
     return
