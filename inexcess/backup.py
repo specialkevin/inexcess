6dd01e8f0e04957bf3361708f6336a1a37e655e4
Kevin Harriss <special.kevin@gmail.com>
2013-01-25 15:31:45 -0600

    Removed a unneeded print statement.

--- inexcess/backup.py
+++ inexcess/backup.py
@@ -3,7 +3,6 @@ from inexcess import mongo
 
 def backupFiles():
     backup_paths = inexcess.read_config('paths')
-    print backup_paths
     for key, path in backup_paths:
         mongo.uploadFile(path, 'files')
     return
