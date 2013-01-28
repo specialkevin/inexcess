9aa168b87c6e72a04dbfdb9991802e89fe370415
Kevin Harriss <special.kevin@gmail.com>
2013-01-28 11:38:22 -0600

    Updated it to handle directories as a path instead of just single files.

--- inexcess/backup.py
+++ inexcess/backup.py
@@ -9,7 +9,7 @@ def backupFiles():
     backup_paths = inexcess.read_config('paths')
     for key, path in backup_paths:
         #THIS CRAZY HACK LOOKING THING IS TO GET ME ALL THE FILES
-        files = list(itertools.chain(*[files for root, dirs, files in os.walk(path)]))
+        files = ["{}/{}".format(root, file) for root,file in list(itertools.chain(*[list(itertools.product([root], files)) for root, dirs, files in os.walk(path)]))]
         for single_file in files:
             mongo.uploadFile(single_file, 'files')
     return
